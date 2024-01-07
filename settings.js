f847594721536f96a95d63f2f2af0836a026ab84
Jupiterke <118110048+Jupiterke@users.noreply.github.com>
2024-01-07 12:11:39 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -16,10 +16,10 @@ Creator : @darkmakerofc , @mr-nima-x
 */
 
 module.exports = {
-  OWNER_NUMBER: "94715166712",
-  SUDO_NUMBERS : "94715166712,94719574492",
-  OWNER_NAME: "『 𝙼𝚁 𝙽𝙸𝙼𝙰 』",
-  BOT_NAME : "ＺＵＳＹＣＯ",
+  OWNER_NUMBER: "254756574562",
+  SUDO_NUMBERS : "254756574562,94719574492",
+  OWNER_NAME: "『 {√💔JUPITER}™🕊️ 』",
+  BOT_NAME : "BLACKSURF-MD",
   ZUSYCO_FILE : "ZUSYCO-QR",
   LOGOS: {
     ALIVE: 'https://i.ibb.co/YTWznr9/ZUSYCO-ALIVE-IMAGE-LOWQ.jpg',
